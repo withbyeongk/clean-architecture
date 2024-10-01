79b611c17c4812ab4e8a53052abc92582d77c8bb
withbyeongk <withbyeongk@gmail.com>
2024-10-01 11:12:05 +0900

    init

---
+++ src/test/java/io/hhplus/cleanarchitecture/CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
