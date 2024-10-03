c7d900ea680a2f0fa9f2171e836d2bc86e5f052a
withbyeongk <withbyeongk@gmail.com>
2024-10-04 04:53:22 +0900

    fix: PK 생성방식 설정

--- src/main/java/io/hhplus/cleanarchitecture/domain/entity/Reservation.java
+++ src/main/java/io/hhplus/cleanarchitecture/domain/entity/Reservation.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class Reservation {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reservation_id")
     private Long id;
 
